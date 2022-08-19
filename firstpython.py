4f32ba801b2032b57fbafb29199b547cb72d1d9c
zakariafakhri <91542941+zakariafakhri@users.noreply.github.com>
2022-08-19 03:37:28 +0100

    Update firstpython.py

--- firstpython.py
+++ firstpython.py
@@ -1,3 +1,8 @@
-#Display the output
-
-print("new python file")
+**Zakaria FAKHRI**
+Phd researcher and data scientist
+*I'm interested in data science because I hope to use and contribute with it in my financial academic research*
+## The below should print 'I am grateful to use jupyter notebook'
+print("I am grateful to use jupyter notebook")
+## Write an equation in center using latex
+$$ 10+12=22 $$
+print(10+12)
